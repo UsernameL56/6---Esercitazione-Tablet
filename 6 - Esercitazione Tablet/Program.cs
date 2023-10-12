896b6b65998ca7a09735ffd1f3ba2d257941a3ea
corsi.20188 <corsi.20188@E2-9-PC05>
2023-10-12 10:11:17 +0200

    Aggiungere i file di progetto.

---
+++ 6 - Esercitazione Tablet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6___Esercitazione_Tablet
+{
+    class Tablet
+    {
+        private string _marca;
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
